e80034eed5d38f9f21f14f8f9495f076f3c22241
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-08-02 17:04:37 +0900

    0802 - 주석 추가

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,9 +2,11 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
+    // * map으로 1~12월간의 지출 값에 대한 새로운 배열을 생성
     const dataPointValues = props.dataPoints.map((dataPoint) => {
         return dataPoint.value;
     });
+    // * 해당 배열에서 가장 큰 값을 maximum 값으로 설정
     const totalMaximum = Math.max(...dataPointValues);
 
     console.log(totalMaximum);
